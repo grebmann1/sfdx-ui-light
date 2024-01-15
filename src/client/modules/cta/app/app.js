16f3bcc3549f216a62615905aea1fb38afb5bce1
rebmang1 <guillaume.rebmann@businesspartner.roche.com>
2024-01-15 22:10:39 +0100

    Quick fix cta doc

--- src/client/modules/cta/app/app.js
+++ src/client/modules/cta/app/app.js
@@ -11,7 +11,7 @@ export default class App extends LightningElement {
 
     connectedCallback(){
         this.initHashHandler();
-        this.isSearchOpen = true;
+        //this.isSearchOpen = true;
     }
 
     scrollToTop = () => {
