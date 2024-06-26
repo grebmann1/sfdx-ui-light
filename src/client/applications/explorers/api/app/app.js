920b6678e700a27350a873383d9f5ccce1ee5b69
rebmang1 <guillaume.rebmann@businesspartner.roche.com>
2024-06-26 10:56:32 +0200

    Fix css + Alpha API

--- src/client/applications/explorers/api/app/app.js
+++ src/client/applications/explorers/api/app/app.js
@@ -1,8 +1,23 @@
-import { track } from "lwc";
+import { api,track } from "lwc";
 import { decodeError,isNotUndefinedOrNull } from 'shared/utils';
 import Toast from 'lightning/toast';
 import FeatureElement from 'element/featureElement';
 
+const METHOD = {
+    GET:'GET',
+    POST:'POST',
+    PUT:'PUT',
+    PATCH:'PATCH',
+    DELETE:'DELETE'
+}
+const TABS = {
+    PARAMETERS:'Parameters',
+    RESPONSE:'Response'
+}
+
+const DEFAULT = {
+    HEADER : 'Content-Type: application/json; charset=UTF-8\nAccept: application/json'
+}
 
 export default class App extends FeatureElement {
 
@@ -11,6 +26,12 @@ export default class App extends FeatureElement {
     @track formattedRequests = [];
     @track endpoint = '/services/data/v60.0';
     @track content;
+    @track method = METHOD.GET;
+
+    @track currentTab = TABS.RESPONSE;
+
+    @api header = DEFAULT.HEADER;
+    @api body;
 
 
     connectedCallback(){
@@ -21,13 +42,19 @@ export default class App extends FeatureElement {
     /** Methods  **/
 
     executeAPI = () => {
-        this.endpoint = this.refs.url.value;
-        this.connector.conn.request(`${this.connector.conn.instanceUrl}/${this.endpoint}`,(err, res) => {
+        const request = this.formatRequest();
+        console.log('API Request',request);
+        this.connector.conn.request(request,(err, res) => {
             if(err){
                 Toast.show({
                     label: err.message || 'Error during request',
                     variant:'error',
                     mode:'dismissible'
+                });
+            }else{
+                Toast.show({
+                    label: 'Success Call',
+                    variant:'success',
                 });
             }
             console.log('err',err);
@@ -35,10 +62,64 @@ export default class App extends FeatureElement {
             this.content = res;
         })
     }
-    
 
+    formatRequest = () => {
+        const request = {
+            method: this.method, 
+            url: `${this.connector.conn.instanceUrl}/${this.endpoint}`
+        }
+        if(isNotUndefinedOrNull(this.body)){
+            request.body = this.body;
+        }
+        if(isNotUndefinedOrNull(this.headers)){
+            // Basic headers for now (Using line split)
+        }
+
+
+        return request;
+    }
+
+
+    /** Events **/
+    
+    handleSelectTab = (e) => {    
+        this.currentTab = e.target.value;
+    }
+
+    reset_click = (e) => {
+        this.header = DEFAULT.HEADER;
+    }
+
+    endpoint_change = (e) => {
+        this.endpoint = e.detail.value;
+    }
+
+    method_change = (e) => {
+        this.method = e.detail.value;
+    }
+
+    body_change = (e) => {
+        this.body = e.target.value;
+    }
+
+    header_change = (e) => {
+        this.header = e.target.value;
+    }
 
     /** Getters */
+
+    get isParamsDisplayed(){
+        return this.currentTab === TABS.PARAMETERS;
+    }
+
+    get isResponseDisplayed(){
+        return this.currentTab === TABS.RESPONSE;
+    }
+
+    get method_options(){
+        return Object.keys(METHOD).map(x => ({label:x,value:x}))
+    }
+
     get isExecuteApiDisabled(){
         return false;
     }
