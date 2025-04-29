1fc9c502c341894273a1ddfb2e7e635939c7a4da
grebmann1 <guillaume.rebmann33@gmail.com>
2025-04-29 16:47:15 +0200

    fixe name

--- src/client/components/onboarding/installSteps/InstallSteps.js
+++ src/client/components/onboarding/installSteps/InstallSteps.js
@@ -1,6 +1,7 @@
 import { LightningElement, track } from 'lwc';
 
 export default class installSteps extends LightningElement {
+    
     @track currentStepIndex = 0;
     @track steps = [
         {
